219fc901c86c72e4f4d41eeff0867d6e799e874b
Sarika-5408 <mpsmsarika@gmail.com>
2026-04-07 15:16:44 +0530

    fix routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
+import Register from "./pages/Register";
+import Login from "./pages/Login";import Dashboard from "./pages/Dashboard";
 import CreateResume from "./pages/CreateResume";
 import EditResume from "./pages/EditResume";
 import ResumeResult from "./ResumeResult";
@@ -12,7 +12,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/create" element={<CreateResume />} />
         <Route path="/edit" element={<EditResume />} />
